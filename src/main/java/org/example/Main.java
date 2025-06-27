1cfd6805e10918cb967e8c33ea317b5a03ba025e
Okode100 <alfakings73@gmail.com>
2025-06-28 02:11:25 +0300

    chore: initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+        /*
+        for(int i = 4; i <= 8; i++){
+            System.out.println("i =" + i);
+        }
+        System.out.println(10 >= 10);
+        System.out.println(10 > 10);
+        System.out.println(10 <= 10);
+        System.out.println(10 == 10);
+        System.out.println(10 != 10);
+         */
+        Point pointA = new Point(10, 20);
+        System.out.println("This is: "+ pointA);
+        int[] number = new int[3];
+        number[0] = 22;
+        number[1] = 20;
+        number[2] = 30;
+        System.out.println(
+                Arrays.toString(number)
+        );
+    }
+}
